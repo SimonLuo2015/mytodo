fc42ee0f075b0a169b8969272b3df6ca1c135940
simonluo2015 <luoshujian1987@163.com>
2018-09-22 22:45:44 +0800

    第五节课基本完成，最后的输入框的验证还没有完成，submit跳转错误。

--- app/views/layouts/main.php
+++ app/views/layouts/main.php
@@ -10,14 +10,14 @@
     <div class="navbar navbar-inverse navbar-fixed-top">
         <div class="navbar-inner">
             <div class="container-fluid">
-                <a class="brand" href="#">myTodo</a>
+                <a class="brand" href="<?php echo base_url()?>">myTodo</a>
                 <div class="nav-collapse collapse">
                     <p class="navbar-text pull-right">
                         <!--RIGHT TOP CONTENT-->
-
+                        <a href="<?php echo base_url()?>users/register">Register</a>
                     </p>
                     <ul class="nav">
-                        <li><a href="#">Home</a></li>
+                        <li><a href="<?php echo base_url()?>">Home</a></li>
                     </ul>
                 </div><!--/.nav-collapse-->
             </div>
@@ -30,7 +30,7 @@
                 <div class="well sidebar-nav">
                     <div style="margin:0 0 10px 10px;">
                         <!--SIDEBAR CONTENT-->
-
+                        <?php $this->load->view('users/login'); ?>
                     </div>
                 </div><!--/.well-->
             </div><!--/span3-->
