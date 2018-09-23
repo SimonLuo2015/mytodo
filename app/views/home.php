abb3743645df99422e5a653dabd094720997cb63
simonluo2015 <luoshujian1987@163.com>
2018-09-23 17:02:38 +0800

    增加注册成功后flashadata在首页显示的信息。

--- app/views/home.php
+++ app/views/home.php
@@ -1,2 +1,5 @@
+<?php if($this->session->flashdata('registered')): ?>
+    <p class="alert alert-dismissable alert-success"><?php echo $this->session->flashdata('registered'); ?></p>
+<?php endif; ?>
 <h1>Welcome to myTodo!</h1>
 <p>myTodo use a simple and helpful application to help you manage your day to day tasks. You can add as many task lists as you want. myTodo is absolutely free! Have fun.</p> 
