d7ec85d825de6ceacd185b3f8371e65f726f3e94
simonluo2015 <luoshujian1987@163.com>
2018-09-24 22:49:03 +0800

    在list_model中实现获取对应ID的数据函数和创建对应数据的列表的函数功能

--- app/models/list_model.php
+++ app/models/list_model.php
@@ -4,6 +4,17 @@ class List_model extends CI_Model{
         $query = $this->db->get('lists');
         return $query->result();
     }
+
+    public function get_list($id){
+        $query = $this->db->get('lists');
+        $this->db->where('id', $id);
+        return $query->row();
+    }
+
+    public function create_list($data){
+        $insert = $this->db->insert('lists', $data);
+        return $insert;
+    }
 }
 
 
