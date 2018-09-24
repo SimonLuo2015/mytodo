2c205d2c4b1c8b9579689da30af369607e4f4dae
simonluo2015 <luoshujian1987@163.com>
2018-09-24 16:51:29 +0800

    调整list_name，list_body展示效果，增加超链接。

--- app/views/lists/index.php
+++ app/views/lists/index.php
@@ -2,7 +2,10 @@
 <p>These are your current lists.</p>
 <ul class='list_items'>
 <?php foreach($lists as $list): ?>
-    <li><?php echo $list->list_name; ?></li>
+    <li>
+        <div class='list_name'><a href="<?php echo base_url();?>lists/show/<?php echo $list->id; ?>"><?php echo $list->list_name; ?></a></div>
+        <div class='list_body'><?php echo $list->list_body; ?></div>
+    </li>
 <?php endforeach; ?>
 </ul>
 <br>
