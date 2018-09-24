14a99655cb79051e1666e10d1f19c96a3a927993
simonluo2015 <luoshujian1987@163.com>
2018-09-24 22:48:20 +0800

    增加lists控制器中的show函数

--- app/controllers/lists.php
+++ app/controllers/lists.php
@@ -18,6 +18,43 @@ class Lists extends CI_Controller{
         $data['main_content'] = 'lists/index';
         $this->load->view('layouts/main', $data);
     }
+
+    public function show($id){
+        // Get all lists from the model.
+        $data['list'] = $this->List_model->get_list($id);
+        // Get all completed tasks for this list.
+        // $data['completed_tasks'] = $this->List_model->get_list_tasks($id, TRUE);
+        // Get all uncompleted tasks for this list
+        // $data['uncompleted_tasks'] = $this->List_model->get_list_tasks($id, FALSE);
+
+        // Load view and layout
+        $data['main_content'] = 'lists/show';
+        $this->load->view('layouts/main', $data);
+    }
+
+    public function add(){
+        $this->form_validation->set_rules('list_name', 'List Name', 'trim|required|xss_clean');
+        $this->form_validation->set_rules('list_body', 'List Body', 'trim|xss_clean');
+
+        if($this->form_validation->run() == FALSE){
+            // Load view and layout
+            $data['main_content'] = 'lists/add_list';
+            $this->load->view('layouts/main', $data);
+        } else {
+            //Validation has ran and passed
+            //Post values to array
+            $data = array(
+                'list_name'     => $this->input->post('list_name'),
+                'list_body'     => $this->input->post('list_body'),
+                'list_user_id'  => $this->session->userdata('user_id')
+            );
+            if($this->List_model->create_list($data)){
+                $this->session->set_flashdata('list_created', 'Your task list has been created.');
+                // Redirected to index page with error above.
+                redirect('lists/index');
+            }
+        }
+    }
 }
 
 ?>
