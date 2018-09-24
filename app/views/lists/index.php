e18e80e86c628eb431282b9affac6b5a96a06e1e
simonluo2015 <luoshujian1987@163.com>
2018-09-24 16:24:20 +0800

    完善view文件，将检索得到的$lists数据进行展示。

--- app/views/lists/index.php
+++ app/views/lists/index.php
@@ -1,1 +1,9 @@
-This is the list index.
+<h1>Lists</h1>
+<p>These are your current lists.</p>
+<ul class='list_items'>
+<?php foreach($lists as $list): ?>
+    <li><?php echo $list->list_name; ?></li>
+<?php endforeach; ?>
+</ul>
+<br>
+<p><a href="#">Create a New List</a></p>
