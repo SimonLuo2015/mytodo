fd4216e6a9e95ad52ec60790e33f177f6982ae85
simonluo2015 <luoshujian1987@163.com>
2018-09-24 15:35:12 +0800

    新建lists控制器

---
+++ app/controllers/lists.php
@@ -0,0 +1,21 @@
+<?php 
+class Lists extends CI_Controller{
+    public function __construct(){
+        parent::__construct();
+
+        if(!$this->session->userdata('logged_in')){
+            $this->session->set_flashdata('noaccess', 'Sorry, you are not logged in.');
+            redirect('home/index');
+        }
+    }
+
+    public function index(){
+        $user_id = $this->session->userdata('user_id');
+
+        //Load view and layout
+        $data['main_content'] = 'lists/index';
+        $this->load->view('layouts/main', $data);
+    }
+}
+
+?>
