0394791630c9ccc35c6bafe5a1f24d07cd9f5d7e
simonluo2015 <luoshujian1987@163.com>
2018-09-24 10:09:16 +0800

    在user控制器下增加login的function，用于验证用户登录情况。

--- app/controllers/users.php
+++ app/controllers/users.php
@@ -21,6 +21,41 @@ class Users extends CI_Controller{
         }
 
     }
+
+    public function login(){
+        $this->form_validation->set_rules('username', 'Username', 'trim|required|min_length[4]|xss_clean');
+        $this->form_validation->set_rules('password', 'Password', 'trim|required|max_length[50]|min_length[4]|xss_clean');
+
+
+        if($this->form_validation->run() == FALSE){
+            //nothing
+        } else {
+            // Get form post
+            $username = $this->input->post('username');
+            $password = $this->input->post('password');
+
+            // Get user id from model
+            $user_id = $this->User_model->login_user($username, $password);
+
+            // Validate user
+            if($user_id){
+                // Create array of user data
+                $user_data = array(
+                    'user_id' => $user_id,
+                    'username' => $username,
+                    'logined_in' => TRUE
+                );
+                // Set session userdata
+                $this->session->set_userdata($user_data);
+
+                redirect('home/index');
+            } else {
+                // Set error
+                $this->session->set_flashdata('login_failed','Sorry, the login info that you entered is invalid.');
+                redirect('home/index');
+            }
+        }
+    }
 }
 
 ?>
