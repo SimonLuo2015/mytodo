2a9d1bef87e04d36d32f613ee6aa6c996f02f8b7
simonluo2015 <luoshujian1987@163.com>
2018-09-29 23:25:00 +0800

    增加对flashdata的信息获取和展示

--- app/views/lists/show.php
+++ app/views/lists/show.php
@@ -5,6 +5,21 @@
     <li><a onclick="return confirm('Are you sure?')" href="<?php echo base_url() ?>lists/delete/<?php echo $list->id; ?>">Delete List</a></li>
 </ul>
 <h1><?php echo $list->list_name; ?></h1>
+<?php if($this->session->flashdata('task_deleted')): ?>
+    <?php echo '<p class="text-success">' .$this->session->flashdata('task_deleted'). '</p>'; ?>
+<?php endif; ?>
+<?php if($this->session->flashdata('task_created')): ?>
+    <?php echo '<p class="text-success">' .$this->session->flashdata('task_created'). '</p>'; ?>
+<?php endif; ?>
+<?php if($this->session->flashdata('task_updated')): ?>
+    <?php echo '<p class="text-success">' .$this->session->flashdata('task_updated'). '</p>'; ?>
+<?php endif; ?>
+<?php if($this->session->flashdata('marked_complete')): ?>
+    <?php echo '<p class="text-success">' .$this->session->flashdata('marked_complete'). '</p>'; ?>
+<?php endif; ?>
+<?php if($this->session->flashdata('marked_new')): ?>
+    <?php echo '<p class="text-success">' .$this->session->flashdata('marked_new'). '</p>'; ?>
+<?php endif; ?>
 Created on: <strong><?php echo date("n-j-Y", strtotime($list->create_date)); ?></strong>
 <br><br>
 <div style="max-width:500px;"><?php echo $list->list_body; ?></div>
