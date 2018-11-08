409a70ef988310a431264bdd1ce38202c1733454
simonluo2015 <luoshujian1987@163.com>
2018-11-09 01:16:21 +0800

    虽然没有必要，但是还是按照视频讲解修改了get_list函数。

--- app/models/list_model.php
+++ app/models/list_model.php
@@ -6,11 +6,23 @@ class List_model extends CI_Model{
     }
 
     public function get_list($id){
+        // $this->db->where('id', $id);
+        // $query = $this->db->get('lists');
+        // // 你妈啊，这里上面两行写反了，导致get_list函数不管输入
+        // // 的id是多少，都直接返回第一条记录。
+        // return $query->row();
+
+        // 上面是原来的写法，说是引起程序报错，所以修改成下面的写法。
+        // 但是我自己验证了下，上面的写法也是ok的。
+        $this->db->select('*');
+        $this->db->from('lists');
         $this->db->where('id', $id);
-        $query = $this->db->get('lists');
-        // 你妈啊，这里上面两行写反了，导致get_list函数不管输入
-        // 的id是多少，都直接返回第一条记录。
-        return $query->row();
+        $query = $this->db->get();
+        if($query->num_rows() != 1){
+            return FALSE;
+        } else {
+            return $query->row();
+        }
     }
 
     public function create_list($data){
@@ -48,14 +60,14 @@ class List_model extends CI_Model{
         $this->db->select('
                             tasks.task_name,
                             tasks.task_body,
-                            tasks.id as tasks_id,
+                            tasks.id as task_id,
                             lists.list_name,
                             lists.list_body
                         ');
         $this->db->from('tasks');
         $this->db->join('lists', 'tasks.list_id = lists.id');
         $this->db->where('tasks.list_id', $list_id);
-        if($acitve == TRUE){
+        if($active == TRUE){
             $this->db->where('tasks.is_complete', 0);
         } else {
             $this->db->where('tasks.is_complete', 1);
